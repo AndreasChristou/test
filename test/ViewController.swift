999c48304f5146040e377b8d722b87ee45c11045
Andreas Christou (s5109900) <s5109900@mac38572.mac.bournemouth.ac.uk>
2019-04-01 11:31:23 +0100

    remove comments

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -1,20 +1,15 @@
-//
-//  ViewController.swift
-//  test
-//
-//  Created by Andreas Christou (s5109900) on 01/04/2019.
-//  Copyright © 2019 Andreas Christou (s5109900). All rights reserved.
-//
-
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
 
 }
+
 
